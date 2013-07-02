346a1f334d948bd40db4e806a0089c96d309b3df
Silviu Pop <thesilviupop@gmail.com>
2013-07-02 13:09:47 +0300

    added ghunit

---
+++ SimpleProject/SimpleProjectTests/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleProjectTests
+//
+//  Created by Silviu Pop on 7/2/13.
+//  Copyright (c) 2013 Silviu Pop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    }
+}
